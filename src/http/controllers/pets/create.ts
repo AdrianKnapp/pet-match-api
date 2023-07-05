801cc7a7601e9a153f9d6227483e6a02e89d41b0
AdrianKnapp <adrknapp@gmail.com>
2023-07-05 07:50:10 -0300

    feat: setup app

---
+++ src/http/controllers/pets/create.ts
@@ -0,0 +1,9 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function create(request: FastifyRequest, response: FastifyReply) {
+  const { name } = request.body as {
+    name: string;
+  };
+
+  response.send({ name });
+}
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { app } from './app'
+
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('🚀 HTTP Server Running')
+  })
