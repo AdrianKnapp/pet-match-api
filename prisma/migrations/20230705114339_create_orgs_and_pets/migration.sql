0ae9b127e801e6afd7d97834c3f84a23b5a65fc2
AdrianKnapp <adrknapp@gmail.com>
2023-07-05 08:46:13 -0300

    chore: setup prisma

---
+++ prisma/migrations/20230705114339_create_orgs_and_pets/migration.sql
@@ -0,0 +1,49 @@
+-- CreateEnum
+CREATE TYPE "Size" AS ENUM ('small', 'medium', 'large');
+
+-- CreateEnum
+CREATE TYPE "Age" AS ENUM ('baby', 'young', 'adult', 'senior');
+
+-- CreateEnum
+CREATE TYPE "Energy" AS ENUM ('lower', 'low', 'medium', 'high', 'higher');
+
+-- CreateEnum
+CREATE TYPE "Independency" AS ENUM ('low', 'medium', 'high');
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "images" TEXT[],
+    "requisites" TEXT[],
+    "age" "Age" NOT NULL,
+    "size" "Size" NOT NULL,
+    "energy" "Energy" NOT NULL,
+    "independency" "Independency" NOT NULL,
+    "environment" "Size" NOT NULL,
+    "orgId" TEXT,
+    "adoptedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orgs" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "zipcode" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "latitude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "orgs_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "orgs"("id") ON DELETE SET NULL ON UPDATE CASCADE;
