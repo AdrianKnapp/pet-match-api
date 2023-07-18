e19f17fe5211fce8f1765282c33e62f4577eb267
AdrianKnapp <adrknapp@gmail.com>
2023-07-18 07:38:52 -0300

    test: get-pet-by-id test

---
+++ src/use-cases/pets/get-pet-by-id.test.ts
@@ -0,0 +1,33 @@
+import { InMemoryPetsRepository } from '../../repositories/in-memory/pets-repository'
+import { describe, it, beforeEach, expect } from 'vitest'
+import { GetPetByIdUseCase } from './get-pet-by-id'
+import { fred } from '@/utils/mocks/pets'
+import { ResourceNotFoundError } from '../errors/resource-not-found-error'
+
+let petsRepository: InMemoryPetsRepository
+let sut: GetPetByIdUseCase
+
+describe('Create Pet By Id Use Case', () => {
+  beforeEach(() => {
+    petsRepository = new InMemoryPetsRepository()
+    sut = new GetPetByIdUseCase(petsRepository)
+  })
+
+  it('should be able to get a pet by id', async () => {
+    const petCreated = await petsRepository.create(fred)
+
+    const { pet } = await sut.execute({
+      petId: petCreated.id,
+    })
+
+    expect(pet.name).toEqual(fred.name)
+  })
+
+  it('should not be able to get pet with wrong city', async () => {
+    await expect(() =>
+      sut.execute({
+        petId: 'wrong-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
