7866323c87a967f6042093c402ffe8613b4ce1af
AdrianKnapp <adrknapp@gmail.com>
2023-08-11 08:29:30 -0300

    feat: added authenticate use-case

---
+++ src/use-cases/orgs/authenticate.ts
@@ -0,0 +1,38 @@
+import { OrgsRepository } from '@/repositories/orgs-repository'
+import { Org } from '@prisma/client'
+import { InvalidCredentialsError } from '../errors/invalid-credentials-error'
+import { compare } from 'bcryptjs'
+
+export interface AuthenticateOrgUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateOrgUseCaseResponse {
+  org: Org
+}
+
+export class AuthenticateOrgUseCase {
+  constructor(private orgsRepository: OrgsRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateOrgUseCaseRequest): Promise<AuthenticateOrgUseCaseResponse> {
+    const org = await this.orgsRepository.findByEmail(email)
+
+    if (!org) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatch = await compare(password, org.password_hash)
+
+    if (!doesPasswordMatch) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      org,
+    }
+  }
+}
