fdc519c2a5e81f2911bbcfbd8bc3cd4f1ce0e540
AdrianKnapp <adrknapp@gmail.com>
2023-08-24 08:38:24 -0300

    feat: authenticate e2e

---
+++ src/http/controllers/orgs/authenticate.test.ts
@@ -0,0 +1,35 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/orgs/register').send({
+      email: 'test@gmail.com',
+      password: 'test123',
+      address: 'Juscelino Kubitschek',
+      name: 'Knapp Org',
+      phone: '51914723123',
+      zipcode: '06472310',
+    })
+
+    const response = await request(app.server).post('/orgs/authenticate').send({
+      email: 'test@gmail.com',
+      password: 'test123',
+    })
+    console.log("🚀 ~ file: authenticate.test.ts:28 ~ response ~ response:", response)
+
+    expect(response.statusCode).toEqual(200)
+    // expect(response.body).toEqual({
+    //   token: expect.any(String),
+    // })
+  })
+})
